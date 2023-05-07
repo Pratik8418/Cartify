a5b6dcb047e2bdad5f2919d9b968fe79985cbf44
Pratik Goti <88868354+Pratik8418@users.noreply.github.com>
2023-05-07 13:28:34 +0530

    Sorting Products

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,4 +1,3 @@
-const { query } = require('express');
 const Product = require('../models/productModel')
 const asyncHandler = require('express-async-handler')
 const slugify = require('slugify')
@@ -39,11 +38,19 @@ const getAllProduct = asyncHandler(
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     
-    const query = Product.find(JSON.parse(queryStr));
-    const products = await query;
-
-
+    let query = Product.find(JSON.parse(queryStr));
     
+    //Sorting
+
+    if(req.query.sort){
+        const sortBy = req.query.sort.split(',').join(" ");
+        query = query.sort(sortBy);
+    }else{
+        query = query.sort('-createdAt');
+    }
+
+
+    const products = await query;
     res.json(products)
      }catch(error){
     throw new Error(error);
